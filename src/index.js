90dc3975d0cef7bb13669fe1cf9cd82e3d0942da
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-05-31 23:15:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './js/menu-theme-swicher.js';
+import dishMenuTemplate from './handlebars/dish-menu-markup.hbs';
+import dishList from './menu.json';
+
+const menu = document.querySelector('.js-menu');
+menu.innerHTML = dishMenuTemplate(dishList);
